63c742e771a5686e1d48762edb0fc2eb7cf5c42f
valentinlibouton <valentin.libouton.git.des8s@8shield.net>
2024-07-21 12:36:09 +0200

    add fonction to retrive path for developpmente

--- __main__.py
+++ __main__.py
@@ -1,6 +1,22 @@
 from emails_managment.email_processing import EmailProcessing
-from tabulate import tabulate
-path = input("path: ")
-ep = EmailProcessing(path=path, with_attachments=True)
-df = ep.get_emails()
-print(df)
+
+
+def paths_to_dict():
+    """This is a temporary function for project development..."""
+    paths = {}
+    with open('paths.txt', 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line:
+                k, v = line.split(' ',1)
+                paths[k] = v.strip()
+    return paths
+
+
+
+if __name__ == "__main__":
+    path = paths_to_dict()['emails_eml']
+    ep = EmailProcessing(path=path, with_attachments=True)
+    df = ep.get_emails()
+    print(df)
+    print(ep.log_execution_time)
